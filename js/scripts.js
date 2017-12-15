cb730022ee46d656313b0bd261ba95e020ae936d
C Knight Johnson <cknightjohnson@gmail.com>
2017-12-15 15:31:23 -0800

    add jackalope png transition

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
   $('#gallery-area').hide();
+  var whatsClicked = "card";
   $('nav a').click(function(){
-    var whatsClicked = $(this).attr('id');
-    $('section').each(function(){
-      if ($(this).attr('id') !== whatsClicked + '-area') {
-      $(this).slideUp();  
-      }
-    });
-    $('#' + whatsClicked + '-area').slideDown();
+    if (whatsClicked !== $(this).attr('id')) {
+      $('#jackalope').toggleClass('jackalope-gallery jackalope-card');
+      whatsClicked = $(this).attr('id');
+      $('section').each(function(){
+        if ($(this).attr('id') !== whatsClicked + '-area') {
+        $(this).slideUp();
+        }
+      });
+      $('#' + whatsClicked + '-area').slideDown();
+    }
   });
 });
